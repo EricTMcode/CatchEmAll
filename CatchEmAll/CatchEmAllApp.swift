887ee592ebd03802bcd4d07f6d913b504f3e68f5
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-05-03 14:05:52 +0200

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Eric on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
